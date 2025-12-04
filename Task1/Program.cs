063635bdea52ba22c896faf4f29890a1c46b95ce
Bipulz <bhandaribipul345@gmail.com>
2025-12-04 10:12:35 +0545

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+    public class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        public double GetArea() => length * breadth;
+        public string ShowDetails() => $"Length: {length}, Breadth: {breadth}";
+        public double GetPerimeter() => 2 * (length + breadth);
+
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Length = 2;
+            rectangle.Breadth = 3;
+
+            Console.WriteLine("---- Rectangle Details ----");
+            Console.WriteLine(rectangle.ShowDetails());
+            Console.WriteLine($"Area: {rectangle.GetArea()}");
+            Console.WriteLine($"Perimeter: {rectangle.GetPerimeter()}");
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
